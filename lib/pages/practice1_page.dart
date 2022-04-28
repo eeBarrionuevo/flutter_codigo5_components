75d278434d2d71e3d13eb4e45599a1c9b25ec7e9
Mandarina <idcareg.g@gmail.com>
2022-04-27 21:21:52 -0500

    Item 1

--- lib/pages/practice1_page.dart
+++ lib/pages/practice1_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class Practice1Page extends StatelessWidget {
@@ -7,6 +5,62 @@ class Practice1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xffF1F1F1),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 260.0,
+              // color: Colors.lightBlueAccent,
+              alignment: Alignment.center,
+              child: Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.black.withOpacity(0.7),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(
+                    Icons.search,
+                    color: Colors.black.withOpacity(0.7),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0,),
+              decoration: BoxDecoration(
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.wifi,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Connections"),
+                      Text("Wi-fi Bluetooth Flight mode"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
