def1234e2e2de1456ed6aaedc1929343bd03dd00
Mandarina <idcareg.g@gmail.com>
2022-04-27 20:32:11 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_components/pages/practice1_page.dart';
+
+void main(){
+  runApp(MyApp(),);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Components App",
+      home: Practice1Page(),
+    );
+  }
+}
---
+++ lib/pages/practice1_page.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class Practice1Page extends StatelessWidget {
+  const Practice1Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
