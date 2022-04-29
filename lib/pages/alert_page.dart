7013a3522d370bed0950d35b89a043dd9a6bd232
Mandarina <idcareg.g@gmail.com>
2022-04-28 21:39:41 -0500

    Alert Page 2

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,11 +1,40 @@
-
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
-  const AlertPage({Key? key}) : super(key: key);
+
+
+  void showMyAlert(BuildContext matasquita) {
+    showDialog(
+      context: matasquita,
+      builder: (BuildContext federico){
+        return Text("Hola");
+      },
+    );
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext mandarina) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("Alert Page"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert(mandarina);
+              },
+              child: Text(
+                "Show Alert",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
