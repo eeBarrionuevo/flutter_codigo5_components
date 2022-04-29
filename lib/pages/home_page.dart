12a740603f3cc508cb19e97a622de7c714d06fe0
Mandarina <idcareg.g@gmail.com>
2022-04-28 19:39:29 -0500

    Item 3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,81 +9,111 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              height: 200.0,
-              width: 200.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18.0),
-                image: DecorationImage(
-                  // image: NetworkImage("https://ih1.redbubble.net/image.622748019.0162/pp,504x498-pad,600x600,f8f8f8.u1.jpg",),
-                  image: NetworkImage(
-                    "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle.jpg",
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 200.0,
+                width: 200.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18.0),
+                  image: const DecorationImage(
+                    // image: NetworkImage("https://ih1.redbubble.net/image.622748019.0162/pp,504x498-pad,600x600,f8f8f8.u1.jpg",),
+                    image: NetworkImage(
+                      "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle.jpg",
+                    ),
                   ),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.12),
-                    offset: Offset(4, 4),
-                    blurRadius: 12,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Flutter Components",
-              style: GoogleFonts.poppins(
-                fontSize: 22.0,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            const SizedBox(
-              width: 160.0,
-              child: Divider(
-                thickness: 0.45,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 12.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.08),
-                    offset: Offset(4, 4),
-                    blurRadius: 12.0,
-                  ),
-                ],
-              ),
-              child: ListTile(
-                leading: Icon(
-                  Icons.check_circle_outline,
-                  color: Color(0xff4A5467),
-                ),
-                title: Text(
-                  "Avatar",
-                  style: GoogleFonts.poppins(),
-                ),
-                subtitle: Text(
-                  "Ir al detalle de Avatar",
-                  style: GoogleFonts.poppins(
-                    fontSize: 13.0,
-                  ),
-                ),
-                trailing: Icon(
-                  Icons.chevron_right,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.12),
+                      offset: Offset(4, 4),
+                      blurRadius: 12,
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Flutter Components",
+                style: GoogleFonts.poppins(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(
+                width: 160.0,
+                child: Divider(
+                  thickness: 0.45,
+                ),
+              ),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+              ItemComponentWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+class ItemComponentWidget extends StatelessWidget {
+  const ItemComponentWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 7.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            offset: Offset(4, 4),
+            blurRadius: 12.0,
+          ),
+        ],
+      ),
+      child: ListTile(
+        leading: Icon(
+          Icons.check_circle_outline,
+          color: Color(0xff4A5467),
+        ),
+        title: Text(
+          "Avatar",
+          style: GoogleFonts.poppins(),
+        ),
+        subtitle: Text(
+          "Ir al detalle de Avatar",
+          style: GoogleFonts.poppins(
+            fontSize: 13.0,
+          ),
+        ),
+        trailing: Icon(
+          Icons.chevron_right,
         ),
       ),
     );
