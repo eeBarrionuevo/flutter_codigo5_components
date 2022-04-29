ec2bb84b962d174bf7a10c0aad851d6278e939af
Mandarina <idcareg.g@gmail.com>
2022-04-28 22:06:57 -0500

    Alert Page 3

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,19 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AlertPage extends StatelessWidget {
-
-
-  void showMyAlert(BuildContext matasquita) {
+  void showMyAlert(BuildContext context) {
     showDialog(
-      context: matasquita,
-      builder: (BuildContext federico){
-        return Text("Hola");
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          // title: Text("My Alert Dialog"),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Check_green_icon.svg/2048px-Check_green_icon.svg.png",
+                height: 60.0,
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Flutter Alert",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(
+                height: 4.0,
+              ),
+              Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                style: GoogleFonts.poppins(
+                  fontSize: 13.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Aceptar",
+              ),
+            ),
+          ],
+        );
       },
     );
   }
 
   @override
-  Widget build(BuildContext mandarina) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -26,7 +71,7 @@ class AlertPage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
-                showMyAlert(mandarina);
+                showMyAlert(context);
               },
               child: Text(
                 "Show Alert",
