0c8d7dc9fdaef1c5b9c71b4ce88264075d2199cb
Mandarina <idcareg.g@gmail.com>
2022-04-29 19:43:01 -0500

    Card Page 1.1

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class CardPage extends StatelessWidget {
@@ -7,6 +5,56 @@ class CardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text(
+          "Card Page",
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 14.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 14.0, vertical: 10.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24.0),
+              // borderRadius: BorderRadius.only(
+              //   topLeft: Radius.circular(20.0),
+              //   topRight: Radius.circular(20.0),
+              // ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.07),
+                  blurRadius: 1,
+                  offset: const Offset(4, 4),
+                ),
+                const BoxShadow(
+                  color: Colors.white,
+                  blurRadius: 5,
+                  offset: Offset(-5, -5),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.60,),
+                    fontSize: 14.0,
+                    height: 1.3,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
