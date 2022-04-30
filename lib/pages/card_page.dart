951a7699c32e7096ded533c0fbfe54b97652a5d4
Mandarina <idcareg.g@gmail.com>
2022-04-29 20:05:22 -0500

    Card Page 2

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CardPage extends StatelessWidget {
   const CardPage({Key? key}) : super(key: key);
@@ -44,10 +45,43 @@ class CardPage extends StatelessWidget {
               children: [
                 Text(
                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                    color: Colors.black.withOpacity(0.60,),
+                    color: Colors.black.withOpacity(
+                      0.60,
+                    ),
                     fontSize: 14.0,
                     height: 1.3,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 12.0,
+                    horizontal: 6.0,
+                  ),
+                  width: double.infinity,
+                  // height: 50.0,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurpleAccent,
+                      borderRadius: BorderRadius.circular(30.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurpleAccent.withOpacity(0.4),
+                          blurRadius: 12,
+                          offset: const Offset(4, 4),
+                        ),
+                      ]),
+                  child: Text(
+                    "Follow me",
+                    style: GoogleFonts.montserrat(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
               ],
