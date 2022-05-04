47f9a244533021f164dad8b499a2e626c381acd6
Mandarina <idcareg.g@gmail.com>
2022-05-03 21:19:22 -0500

    List 3

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-class ListPage extends StatelessWidget {
+class ListPage extends StatefulWidget {
+
+  @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
 
   List<String> superheroes = [
     "Superman",
@@ -10,6 +16,54 @@ class ListPage extends StatelessWidget {
     "Aquaman",
   ];
 
+
+  Map<String, dynamic> teamMap = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+      {
+        "name": "Batman",
+        "image":
+        "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      },
+      {
+        "name": "Superman",
+        "image":
+        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+        "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      }
+    ]
+  };
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,14 +72,36 @@ class ListPage extends StatelessWidget {
         title: Text("ListPage"),
         centerTitle: true,
       ),
+      // body: ListView.builder(
+      //   itemCount: superheroes.length,
+      //   itemBuilder: (BuildContext context, int index){
+      //     return ListTile(
+      //       title: Text(superheroes[index]),
+      //       subtitle: const Text("Nombre"),
+      //       trailing: IconButton(
+      //         icon: Icon(Icons.delete),
+      //         onPressed: (){
+      //           superheroes.removeAt(index);
+      //           setState(() {
+      //
+      //           });
+      //         },
+      //       ),
+      //     );
+      //   },
+      // ),
       body: ListView.builder(
-        itemCount: superheroes.length,
+        itemCount: teamMap["members"].length,
         itemBuilder: (BuildContext context, int index){
           return ListTile(
-            title: Text(superheroes[index]),
-            subtitle: const Text("Nombre"),
+            leading: CircleAvatar(
+              radius: 22,
+              backgroundImage: NetworkImage(teamMap["members"][index]["image"]),
+            ),
+            title: Text(teamMap["members"][index]["name"]),
+            subtitle: Text(teamMap["squadName"]),
             trailing: IconButton(
-              icon: Icon(Icons.delete),
+              icon: Icon(Icons.remove_red_eye_sharp),
               onPressed: (){
 
               },
