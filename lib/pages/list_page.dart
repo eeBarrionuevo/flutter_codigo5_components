8a5101aa4336e9a6203542da0322426860b7b6d7
Mandarina <idcareg.g@gmail.com>
2022-05-03 20:18:52 -0500

    List 2

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,11 +1,38 @@
-
 import 'package:flutter/material.dart';
 
 class ListPage extends StatelessWidget {
-  const ListPage({Key? key}) : super(key: key);
+
+  List<String> superheroes = [
+    "Superman",
+    "Batman",
+    "Wonder Woman",
+    "Flash",
+    "Aquaman",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Text("ListPage"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: superheroes.length,
+        itemBuilder: (BuildContext context, int index){
+          return ListTile(
+            title: Text(superheroes[index]),
+            subtitle: const Text("Nombre"),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: (){
+
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 }
