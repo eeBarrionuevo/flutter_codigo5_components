0347f6cc2099f4449e21b3e7dbff526371092023
Mandarina <idcareg.g@gmail.com>
2022-05-04 19:30:05 -0500

    GridPage 2

--- lib/pages/grid_page.dart
+++ lib/pages/grid_page.dart
@@ -3,10 +3,86 @@
 import 'package:flutter/material.dart';
 
 class GridPage extends StatelessWidget {
-  const GridPage({Key? key}) : super(key: key);
+
+
+  Map<String, dynamic> teamMap = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+      {
+        "name": "Batman",
+        "image":
+        "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      },
+      {
+        "name": "Superman",
+        "image":
+        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+        "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      }
+    ]
+  };
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text("GridPage"),
+        centerTitle: true,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        // padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        childAspectRatio: 1,
+        children: [
+          Container(
+            color: Colors.red,
+            child: Text("Hola 1"),
+          ),
+          Container(
+            color: Colors.greenAccent,
+            child: Text("Hola 2"),
+          ),
+          Container(
+            color: Colors.blue,
+            child: Text("Hola 3"),
+          ),
+          Container(
+            color: Colors.deepPurple,
+            child: Text("Hola 4"),
+          ),
+        ],
+      ),
+    );
   }
 }
