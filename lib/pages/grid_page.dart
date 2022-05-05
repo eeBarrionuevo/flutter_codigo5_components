12693de577e6e3a83eae67bf0b4d2251b7e2c48e
Mandarina <idcareg.g@gmail.com>
2022-05-04 19:52:57 -0500

    GridPage 4

--- lib/pages/grid_page.dart
+++ lib/pages/grid_page.dart
@@ -1,10 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 
 class GridPage extends StatelessWidget {
-
-
   Map<String, dynamic> teamMap = {
     "squadName": "Super hero squad",
     "homeTown": "Metro City",
@@ -15,7 +11,7 @@ class GridPage extends StatelessWidget {
       {
         "name": "Batman",
         "image":
-        "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+            "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
         "age": 29,
         "secretIdentity": "Dan Jukes",
         "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
@@ -23,7 +19,7 @@ class GridPage extends StatelessWidget {
       {
         "name": "Superman",
         "image":
-        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
         "age": 39,
         "secretIdentity": "Jane Wilson",
         "powers": [
@@ -35,7 +31,7 @@ class GridPage extends StatelessWidget {
       {
         "name": "Wonder Woman",
         "image":
-        "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+            "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
         "age": 1000000,
         "secretIdentity": "Unknown",
         "powers": [
@@ -49,7 +45,6 @@ class GridPage extends StatelessWidget {
     ]
   };
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,10 +56,33 @@ class GridPage extends StatelessWidget {
       body: GridView.count(
         crossAxisCount: 2,
         // padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        childAspectRatio: 1,
-        children: teamMap["members"].map<Widget>((e)=>Text(e["name"])).toList(),
+        // mainAxisSpacing: 10,
+        // crossAxisSpacing: 10,
+        // childAspectRatio: 1,
+        children: teamMap["members"]
+            .map<Widget>(
+              (e) => Container(
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  image: DecorationImage(
+                    image: NetworkImage(e['image'],),
+                    fit: BoxFit.cover
+                  ),
+                ),
+                child: Text(
+                  e["name"],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+            .toList(),
       ),
     );
   }
